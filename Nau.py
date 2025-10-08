b85c8e05ca70dcfea87a1d39a3630bca032b663d
unairomero0 <a2510805@institutmontilivi.cat>
2025-10-08 08:45:19 +0200

    Moviments de la nau

--- Nau.py
+++ Nau.py
@@ -16,17 +16,13 @@ def main():
     while not sortir:
         op = input('Entra una opció: ')
         if op=='d':
-            pass
-            #sumar 1 a la variable posX
+            posX = posX + 1
         elif op=='a':
-            pass
-            #restar 1 a la variable posX
+            posX = posX - 1
         elif op=='w':
-            pass
-            #sumar 1 a la variable posY
+            posY = posY + 1
         elif op=='s':
-            pass
-            #restar 1 a la variable posY
+            posY = posY - 1
         elif op=='0':
             sortir=True
             print("Has sortit de la nau")
